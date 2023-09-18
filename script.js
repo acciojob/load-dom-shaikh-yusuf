603285aceac98c203544004ec8a852d4a7866677
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-18 12:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.body.innerHTML = '';
+
+        // Create a new text node with the message
+        var textNode = document.createTextNode("DOM load success");
+
+        // Append the text node to the body
+        document.body.appendChild(textNode);
